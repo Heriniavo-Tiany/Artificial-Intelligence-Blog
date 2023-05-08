861eae0b05322c48b5d08b56db814442be2c86b9
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-05-08 13:51:22 +0300

    Slugify

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const slugify = require('slugify');
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id/:slug', function(req, res, next) {
   const id = req.params.id; // get the :id parameter from the URL
   const query = 'SELECT * FROM articles WHERE id = $1 LIMIT 1';
   const values = [id]; // use the id parameter in the query
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const slugify = require('slugify');
 
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Intelligence Artificielle - Home' });
-  const query = 'SELECT * FROM articles';
+  const query = 'SELECT * FROM articles ORDER BY created_at DESC';
+
   req.db.query(query, (err, result) => {
     if (err) {
       return next(err);
     }
-    res.render('index', {  title: 'Intelligence Artificielle - Home', articles: result.rows });
+
+    const articles = result.rows.map(row => ({
+      ...row,
+      slug: slugify(row.title, { lower: true, strict: true }),
+    }));
+
+    res.render('index', {
+      title: 'Intelligence Artificielle - Accueil',
+      articles,
+      slugify,
+    });
   });
 });
 
