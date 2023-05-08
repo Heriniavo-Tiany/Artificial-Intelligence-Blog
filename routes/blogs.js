cbfe9dc9e669e69b236441c73be058c1542f9b18
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-05-08 12:54:31 +0300

    Links

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
+  const id = req.params.id; // get the :id parameter from the URL
   const query = 'SELECT * FROM articles WHERE id = $1 LIMIT 1';
-  const values = [2];
+  const values = [id]; // use the id parameter in the query
 
   req.db.query(query, values, (err, result) => {
     if (err) {
@@ -13,5 +14,6 @@ router.get('/', function(req, res, next) {
     res.render('blog', { title: 'Intelligence Artificielle - Blog', article: result.rows[0] });
   });
 });
+
 
 module.exports = router;
